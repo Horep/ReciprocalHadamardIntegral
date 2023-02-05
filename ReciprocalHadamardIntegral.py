2c2c28fa8bdbfc7d944e536bbf6642e4cd5605df
Horep <horep245@gmail.com>
2023-02-05 15:27:30 +0000

    Changed output to include error estimation

--- ReciprocalHadamardIntegral.py
+++ ReciprocalHadamardIntegral.py
@@ -1,10 +1,12 @@
 import mpmath as mp
-mp.mp.dps = 200 # number of decimal places used during computation
+mp.mp.dps = 400  # number of decimal places used during computation
 
 
-def h_recip(x):  #defn of Hadamard gamma function
+def h_recip(x):  # defn of Hadamard gamma function
     q = (mp.sin(mp.pi*x) / (2*mp.pi))*(mp.psi(0, x/2) - mp.psi(0, (x+1)/2))
     return 1/(mp.gamma(x) * (1 + q))
 
 
-print(mp.quad(h_recip, (0, mp.inf), error=True))
+w = mp.quad(h_recip, (0, mp.inf), error=True)
+print(w[0])  # value
+print(w[1])  # estimated error
